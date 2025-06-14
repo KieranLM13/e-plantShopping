01133ce7f362b6932594628808b52bd3bcc0af3b
KieranLM13 <kieranmcfall@sky.com>
2025-06-14 02:05:51 -0400

    Updates to cart slice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,34 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+        } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
-    
+    removeItem: (state, action) => {
+        const { name, quantity } = action.payload;
+        state.items = state.items.filter(item => item.name !== name);
+        state.numOfItems -= quantity;
+
+        if (state.numOfItems < 0) {
+            state.numOfItems = 0;
+        }
+    },
+
+    updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+
+        if (existingItem) {
+            const differenceQuantity = quantity - existingItem.quantity;
+            state.numOfItems += differenceQuantity;
+            existingItem.quantity = quantity;
+        }
     },
   },
 });
